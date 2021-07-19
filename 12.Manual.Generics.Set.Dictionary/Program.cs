b83389cd7eda90f534049b2aecab4a4108995be1
marcu <marcu@LAPTOP-MTO8J5A0>
2021-07-19 15:50:05 -0300

    Adicionar arquivos de projeto.

---
+++ 12.Manual.Generics.Set.Dictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace _12.Manual.Generics.Set.Dictionary {
+    class Program {
+        static void Main(string[] args) {
+            PrintService<int> printService = new PrintService<int>(); // ---- > Chamada do PREINTSERVICE
+
+            Console.WriteLine("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+            
+            for(int i =0; i<n; i++) {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+
+        }
+    }
+}
